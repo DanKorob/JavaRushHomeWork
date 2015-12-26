d9a0a85dd86c1f61f2869c0afad0c956327f2144
DanKorob <danilkorobwork@gmail.com>
2015-12-26 10:24:37 +0200

    level12.lesson4 Done!!

---
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    //Напишите тут ваши методы
+    public void print(int x)
+    {}
+    public void print(String x)
+    {}
+    public void print(double x)
+    {}
+    public void print(char x)
+    {}
+    public void print(short x)
+    {}
+
+}
