e2e224c3dc4a0fd6378b3e9690d0810b689ca4cf
DanKorob <danilkorobwork@gmail.com>
2016-01-01 15:04:34 +0200

    level16.lesson10 Done!

---
+++ src/examples/golovach/thread/App.java
@@ -0,0 +1,13 @@
+package examples.golovach.thread;
+
+/**
+ * Created by Sony on 01.01.2016.
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        Thread thread = new Thread();
+        thread.start();
+    }
+}
