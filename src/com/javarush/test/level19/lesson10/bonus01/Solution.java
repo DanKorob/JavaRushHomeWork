e7142b0c3fa67758dd2e59061580eedf29be2d93
Dan <danilkorobwork@gmail.com>
2016-02-26 11:23:45 +0200

    level19.lesson10.bonus1 Done!  (finally!)

--- src/com/javarush/test/level19/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level19/lesson10/bonus01/Solution.java
@@ -1,6 +1,11 @@
 package com.javarush.test.level19.lesson10.bonus01;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /* Отслеживаем изменения
@@ -17,11 +22,11 @@ file1:         file2:             результат:(lines)
 строка3        строка3            SAME строка3
 строка4                           REMOVED строка4
 строка5        строка5            SAME строка5
-строка0                           ADDED строка0
+               строка0            ADDED строка0
 строка1        строка1            SAME строка1
 строка2                           REMOVED строка2
 строка3        строка3            SAME строка3
-строка5                           ADDED строка5
+               строка5            ADDED строка5
 строка4        строка4            SAME строка4
 строка5                           REMOVED строка5
 */
@@ -29,9 +34,72 @@ file1:         file2:             результат:(lines)
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader consolereader = new BufferedReader(new InputStreamReader(System.in));
+        String filename1 = consolereader.readLine();
+        String filename2 = consolereader.readLine();
+        consolereader.close();
+
+        BufferedReader fileReader1 = new BufferedReader(new FileReader(filename1));
+        BufferedReader fileReader2 = new BufferedReader(new FileReader(filename2));
+
+        List<String> list1 = new LinkedList<String>();
+        List<String> list2 = new LinkedList<String>();
+
+        while (fileReader1.ready())
+            list1.add(fileReader1.readLine());
+        while (fileReader2.ready())
+            list2.add(fileReader2.readLine());
+
+        fileReader1.close();
+        fileReader2.close();
+
+        getResult(list1, list2);
     }
 
+    public static void getResult(List<String> list1, List<String> list2) {
+
+        int ind1 = 0;
+        int ind2 = 0;
+
+        while (true)
+        {
+            String s1 = list1.get(ind1);
+            String s2 = list2.get(ind2);
+
+            if (s1.equals(s2))
+            {
+                lines.add(new LineItem(Type.SAME, s1));
+                ++ind1;
+                ++ind2;
+            }
+            else if ((ind2 + 1 < list2.size()) && (s1.equals(list2.get(ind2 + 1))))
+            {
+                lines.add(new LineItem(Type.ADDED, s2));
+                ind2++;
+            }
+            else if ((ind1 + 1 < list1.size()) && (s2.equals(list1.get(ind1 + 1))))
+            {
+                lines.add(new LineItem(Type.REMOVED, s1));
+                ind1++;
+            }
+
+            if (ind1 == list1.size() && ind2 == list2.size())
+                break;
+            else if (ind2 == list2.size())
+            {
+                lines.add(new LineItem(Type.REMOVED, list1.get(ind1)));
+                break;
+            }
+            else if (ind1 == list1.size())
+            {
+                lines.add(new LineItem(Type.ADDED, list2.get(ind2)));
+                break;
+            }
+        }
+
+    }
 
     public static enum Type {
         ADDED,        //добавлена новая строка
--- src/com/javarush/test/level19/lesson10/bonus01/test/SolutionTest.java
+++ src/com/javarush/test/level19/lesson10/bonus01/test/SolutionTest.java
@@ -12,6 +12,21 @@ import static org.junit.Assert.assertEquals;
 public class SolutionTest
 {
     @Test
+    public void test_0()
+    {
+        assertList(
+                "строка1\n",
+
+                "строка1\n",
+
+                "строка1 SAME"
+        );
+
+        Solution.lines.clear();
+    }
+
+
+    @Test
     public void test_begin1()
     {
         assertList(
@@ -64,7 +79,7 @@ public class SolutionTest
     }
 
     @Test
-    public void test_situation1()
+    public void test_situation_1()
     {
         assertList(
                 "строка5\n" +
@@ -86,7 +101,29 @@ public class SolutionTest
 
     @Ignore
     @Test
-    public void testN() {
+    public void test_situation_2()
+    {
+        assertList(
+                "строка5\n" +
+                "строка12\n" +
+                "строка6",
+
+                "строка5\n" +
+                "строка6\n" +
+                "строка12",
+
+                "строка5 SAME\n" +
+                "строка12 REMOVED\n" +
+                "строка6 SAME\n" +
+                "строка12 ADDED"
+        );
+
+        Solution.lines.clear();
+    }
+
+
+    @Test
+    public void test_4() {
         assertList(
                 "строка1\n" +
                 "строка2\n" +
@@ -102,15 +139,56 @@ public class SolutionTest
                 "строка8 ADDED\n" +
                 "строка2 SAME\n" +
                 "строка9 ADDED\n" +
-                "строка3 SAME\n"
+                "строка3 SAME"
         );
 
+        Solution.lines.clear();
+    }
 
+    @Test
+    public void test_5() {
+
+        assertList(
+                "строка1\n" +
+                "строка2\n" +
+                "строка3\n" +
+                "строка4\n" +
+                "строка5\n" +
+                "строка1\n" +
+                "строка2\n" +
+                "строка3\n" +
+                "строка4\n" +
+                "строка5",
+
+                "строка1\n" +
+                "строка3\n" +
+                "строка5\n" +
+                "строка0\n" +
+                "строка1\n" +
+                "строка3\n" +
+                "строка5\n" +
+                "строка4",
+
+                "строка1 SAME\n" +
+                "строка2 REMOVED\n" +
+                "строка3 SAME\n" +
+                "строка4 REMOVED\n" +
+                "строка5 SAME\n" +
+                "строка0 ADDED\n" +
+                "строка1 SAME\n" +
+                "строка2 REMOVED\n" +
+                "строка3 SAME\n" +
+                "строка5 ADDED\n" +
+                "строка4 SAME\n" +
+                "строка5 REMOVED"
+        );
+
+        Solution.lines.clear();
     }
 
     private void assertList(String file1, String file2, String expected)
     {
-//        Solution.getCombinedLines(createlist(file1), createlist(file2));
+        Solution.getResult(createlist(file1), createlist(file2));
         String actual = arrayLineItemToString(Solution.lines);
 
         assertEquals(expected, actual);
